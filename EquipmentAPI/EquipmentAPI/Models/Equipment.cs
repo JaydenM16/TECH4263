66d2b218d4b8ae9d184d41c4e29fdda95f323abd
JMoodyy <jayden.moody2001@gmail.com>
2026-02-16 19:57:53 -0600

    Lab 1: In-memory Equipment API Complete

--- EquipmentAPI/EquipmentAPI/Models/Equipment.cs
+++ EquipmentAPI/EquipmentAPI/Models/Equipment.cs
@@ -2,5 +2,20 @@
 {
     public class Equipment
     {
+        private static int _nextId = 1;
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Category { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Location { get; set; } = "";
+
+        public Equipment(string name, string category, string status, string location)
+        {
+            Id = _nextId++;
+            Name = name;
+            Category = category;
+            Status = status;
+            Location = location;
+        }
     }
 }
--- EquipmentAPI/EquipmentAPI/Program.cs
+++ EquipmentAPI/EquipmentAPI/Program.cs
@@ -1,3 +1,5 @@
+using EquipmentAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,16 +18,36 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/createequipment", (string name, string category, string status, string location) => { ... })
+var equipmentList = new List<Equipment>();
+
+app.MapPost("/createequipment", (string name, string category, string status, string location) => 
+{
+    var equipment = new Equipment(name, category, status, location);
+
+    equipmentList.Add(equipment);
+
+    return Results.Created($"/createequipment/{equipment.Id}", equipment);
+})
    .WithName("CreateEquipment")
    .WithOpenApi();
 
 
-app.MapGet("/getequipments", () => { ... })
+app.MapGet("/getequipments", () => 
+{
+    return Results.Ok(equipmentList);
+})
    .WithName("GetEquipments")
    .WithOpenApi();
 
-app.MapGet("/getequipment/{id}", (int id) => { ... })
+app.MapGet("/getequipment/{id}", (int id) => 
+{
+    var equipment = equipmentList.FirstOrDefault(e => e.Id == id);
+    if (equipment == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(equipment);
+})
    .WithName("GetEquipmentById")
    .WithOpenApi();
 
